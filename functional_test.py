e7f89191bf3f69b1a7ccb3303a3d6011baf63757
aluruguna <aluruguna@gmail.com>
2016-11-13 22:21:14 -0800

    [#133343434] Add

--- functional_test.py
+++ functional_test.py
@@ -4,9 +4,5 @@ browser.get('http://localhost:8000')
 assert 'Django' in browser.title
 
 print("test")
-<<<<<<< HEAD
-for i in range(10):
-    print(i)
-=======
 print("test2")
->>>>>>> 4c5bbc0ac304ce633ac61c649881f53669f7ba2d
+
